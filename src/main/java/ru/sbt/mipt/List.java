201757708b6a7d22e4a869513c113c487edc7432
Ilya_37 <kolobov_ia@mail.ru>
2015-11-22 21:15:11 +0300

    Initial commit

--- src/main/java/ru/sbt/mipt/List.java
+++ src/main/java/ru/sbt/mipt/List.java
@@ -1,8 +1,9 @@
 package ru.sbt.mipt;
+import java.io.Writer;
+import static java.lang.System.out;
 
 /**
  * Created by Ilya on 22.11.2015.
  */
 public class List {
-
 }
---
+++ src/main/java/ru/sbt/mipt/Main.java
@@ -0,0 +1,10 @@
+package ru.sbt.mipt;
+
+/**
+ * Created by Ilya on 22.11.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
